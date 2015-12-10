1abca570864d72c4382a4d5100247a54d75c3676
Maxim Zabelin <zabelin@api.ai>
2015-12-10 19:46:45 +0600

    OSX demo project has been linked against AI framework.

--- AIOSXDemo/AIOSXDemo/AppDelegate.swift
+++ AIOSXDemo/AIOSXDemo/AppDelegate.swift
@@ -3,21 +3,19 @@
 //  AIOSXDemo
 //
 
+import AI
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        AI.configure("YOUR_CLIENT_ACCESS_TOKEN", "YOUR_SUBSCRIPTION_KEY")
+        AI.configure("3485a96fb27744db83e78b8c4bc9e7b7", "cb9693af-85ce-4fbf-844a-5563722fc27f")
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
