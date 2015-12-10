9bfed1866c4f38ef6b83c3ff056631a019af41e3
Maxim Zabelin <zabelin@api.ai>
2015-12-10 16:59:46 +0600

    OSX demo has been set up.

---
+++ AIOSXDemo/AIOSXDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AIOSXDemo
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
